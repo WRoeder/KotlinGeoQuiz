8dd2a0b6719c36a7b05a19e6c1ff06fbc9e23013
WRoeder <77704216+WRoeder@users.noreply.github.com>
2022-02-24 18:47:47 -0500

    2/24/22 Changes

--- app/src/main/java/com/jcallinan/kotlingeoquiz/CheatActivity.kt
+++ app/src/main/java/com/jcallinan/kotlingeoquiz/CheatActivity.kt
@@ -1,5 +1,6 @@
 package com.jcallinan.kotlingeoquiz
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+const val EXTRA_ANSWER_SHOWN = "com.jcallinan.kotlingeoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.jcallinan.kotlingeoquiz.answer_is_true"
 
@@ -31,7 +33,14 @@ class CheatActivity : AppCompatActivity() {
                 else -> R.string.false_button
             }
             answerTextView.setText(answerText)
+            setAnswerShownResult(true)
         }
+    }
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
     }
     companion object {
         fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
--- app/src/main/java/com/jcallinan/kotlingeoquiz/MainActivity.kt
+++ app/src/main/java/com/jcallinan/kotlingeoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jcallinan.kotlingeoquiz
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,6 +14,7 @@ import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -63,12 +65,21 @@ class MainActivity : AppCompatActivity() {
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             // val intent = Intent(this, CheatActivity::class.java)
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
 
         updateQuestion()
 
 
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK){
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
     }
     private fun updateQuestion() {
         val questionTextResId = quizViewModel.currentQuestionText
@@ -76,10 +87,10 @@ class MainActivity : AppCompatActivity() {
     }
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
 
